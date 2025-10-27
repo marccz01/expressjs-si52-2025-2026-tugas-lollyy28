873c3c2062a150b2befcd23b6c336a419ec0fee2
lollyy28 <nelson.marc5678@gmail.com>
2025-10-27 23:10:22 +0700

    Update profileController.js

--- controller/profileController.js
+++ controller/profileController.js
@@ -1,25 +1,25 @@
-import UserModel from "../model/userModel.js";
+export const publicProfile = (req, res) => {
+const username = req.params.username
+    res.render('public-profile', {
+        title : username,
+        username : username,
+        bio: "test 1234"
+    })
+}
 
 export const privateProfile = async (req, res) => {
     try {
-        const userId = req.user?.id;
+        const user = req.user;
 
-        const user = await UserModel.findOne(userId);
-
-        if (!user) {
-            res.status(404).json({
-            message : "user not found",
-            data : null,
-        });
-        }
         res.status(200).json({
-            message : "Private profile berhasil di akses",
-            data : null,
+            message: "Private profile berhasil di akses",
+            data: user,
         });
-    } catch (error){
+        
+    } catch (error) {
         res.status(500).json({
-            message : error.message,
-            data : null,
+            message: error.message,
+            data: null,
         });
     }
 };
